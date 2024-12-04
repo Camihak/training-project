222bb1fab7e6f880035225f1ddba27e6636739f1
Камила <105364337+Camihak@users.noreply.github.com>
2024-12-04 11:30:13 +0300

    Рефакторит код мини-игры: "Угадай число"

--- game-guess-number.js
+++ game-guess-number.js
@@ -1,10 +1,6 @@
-const guessesNumber = (minNumber, maxNumber) => {
-  return Math.floor(((maxNumber - minNumber) / 2) + minNumber);
-};
-
-const checksGuessedNumber = (guessNumber, minNumber, maxNumber) => {
+const guessesNumber = (guessNumber, minNumber, maxNumber) => {
   let flag = true;
-  let adjacentRangeValue = guessesNumber(minNumber, maxNumber);
+  let adjacentRangeValue = Math.floor(((maxNumber - minNumber) / 2) + minNumber);
   while (guessNumber != adjacentRangeValue) {
     if (guessNumber < adjacentRangeValue) {
       flag = false;
@@ -16,21 +12,21 @@ const checksGuessedNumber = (guessNumber, minNumber, maxNumber) => {
         console.log(`Компьютер 1: Больше.`);
         ++adjacentRangeValue;
     } else if (guessNumber > adjacentRangeValue && flag) {
-      flag = false;
-      console.log(`Компьютер 2: Пробую число ${adjacentRangeValue}`);
-      console.log(`Компьютер 1: Больше.`);
-      adjacentRangeValue = Math.floor(adjacentRangeValue * 2);
-    } else if (guessNumber < adjacentRangeValue && !flag) {
-      console.log(`Компьютер 2: Пробую число ${adjacentRangeValue}`);
-      console.log(`Компьютер 1: Меньше.`);
-      --adjacentRangeValue;
+        flag = false;
+        console.log(`Компьютер 2: Пробую число ${adjacentRangeValue}`);
+        console.log(`Компьютер 1: Больше.`);
+        adjacentRangeValue = Math.floor(adjacentRangeValue * 2);
+    } else {
+        console.log(`Компьютер 2: Пробую число ${adjacentRangeValue}`);
+        console.log(`Компьютер 1: Меньше.`);
+        --adjacentRangeValue;
     }
   }
   console.log(`Компьютер 2: Пробую число ${adjacentRangeValue}`)
   console.log(`Компьютер 1: Угадал!`)
 }
 
-console.log(checksGuessedNumber(5, 1, 100));
-console.log(checksGuessedNumber(10, 1, 100));
-console.log(checksGuessedNumber(15, 1, 100));
-console.log(checksGuessedNumber(20, 1, 100));
+console.log(guessesNumber(5, 1, 100));
+console.log(guessesNumber(6, 10, 20));
+console.log(guessesNumber(17, 11, 31));
+console.log(guessesNumber(20, 1, 100));
